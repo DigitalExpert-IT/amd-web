7b096978270c74895d523b879bde5b08aec3d6ff
Y0SH4 <yosa.agung13@gmail.com>
2023-11-24 03:01:46 +0700

    update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,7 @@
-import { ReactNode } from 'react'
-
+import Link from "next/link";
+import { useTranslation } from "react-i18next";
+import { FOOTER_DATA } from "constant/Dummy";
+import { BiLogoInstagram, BiLogoWhatsapp, BiLogoTwitter } from "react-icons/bi";
 import {
   Box,
   Container,
@@ -7,64 +9,78 @@ import {
   Stack,
   Text,
   Image,
-} from '@chakra-ui/react'
-
-import { useTranslation } from 'react-i18next'
-import { FOOTER_DATA } from 'constant/Dummy'
-import Link from 'next/link'
-
-const ListHeader = ({ children }: { children: ReactNode }) => {
-  return (
-    <Text fontWeight={'500'} fontSize={'lg'} mb={2}>
-      {children}
-    </Text>
-  )
-}
+  Icon,
+} from "@chakra-ui/react";
 
 const Footer = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <Box bg="rgba(26, 26, 26, 0.40)">
       <Container as={Stack} maxW="container.xl" py={10}>
         <SimpleGrid
-          templateColumns={{ sm: '1fr 1fr', md: '8fr 3fr 4fr' }}
-          spacing={8}>
+          templateColumns={{ sm: "1fr 1fr", md: "7fr 3fr 4fr" }}
+          spacing={8}
+        >
           <Stack spacing={6}>
             <Box>
-              <Image src="https://ik.imagekit.io/msxxxaegj/asetmasadepan/amd-logo.svg?updatedAt=1700720379734" objectFit="cover" />
+              <Image
+                src="https://ik.imagekit.io/msxxxaegj/asetmasadepan/amd-logo.svg?updatedAt=1700720379734"
+                objectFit="cover"
+                alt="logo-image"
+              />
             </Box>
             <Box w={{ base: "100%", md: "70%" }}>
-              <Text bgGradient='linear-gradient(91deg, #9795FF 1.74%, #FFF 43.71%, #FFF 61.62%, #BE9FFF 102.48%)'
-                bgClip="text">{t("commons.footerDescription")}</Text>
+              <Text
+                bgGradient="linear-gradient(91deg, #9795FF 1.74%, #FFF 43.71%, #FFF 61.62%, #BE9FFF 102.48%)"
+                bgClip="text"
+              >
+                {t("commons.footerDescription")}
+              </Text>
             </Box>
           </Stack>
           {FOOTER_DATA.map((items, idx) => (
-            <Stack align={'flex-start'} key={idx}>
-              <ListHeader>{items.headList}</ListHeader>
-              {items.children.map(obj => (
-                <Link href={obj.link}>
+            <Stack align={"flex-start"} key={idx}>
+              <Text fontWeight={"500"} fontSize={"lg"} mb={2}>
+                {items.headList}
+              </Text>
+              {items.children.map((obj, id) => (
+                <Link href={obj.link} key={id}>
                   <Text fontSize="sm">{obj.name}</Text>
                 </Link>
               ))}
             </Stack>
           ))}
-
         </SimpleGrid>
-        <Stack direction="row" justify="space-between" w="100%" bg="green">
+        <Stack
+          direction={{ base: "column-reverse", md: "row" }}
+          justify="space-between"
+          w="100%"
+          mt="2rem"
+          align="center"
+        >
           <Box>
-            <Text fontSize="xs">&#169; {new Date().getFullYear()} PT. Aset Masa Depan. All right reserved</Text>
+            <Text fontSize="xs">
+              &#169; {new Date().getFullYear()} PT. Aset Masa Depan. All right
+              reserved
+            </Text>
           </Box>
           <Box display="flex">
-            <Text>Privacy</Text>
-            <Text mx="2rem">Terms of Service</Text>
-            <Text>Cookie Policy</Text>
+            <Text fontSize="xs">Privacy</Text>
+            <Text fontSize="xs" mx="2rem">
+              Terms of Service
+            </Text>
+            <Text fontSize="xs">Cookie Policy</Text>
           </Box>
-
+          <Stack spacing="1rem" direction="row">
+            <Icon as={BiLogoInstagram} w={5} h={5} />
+            <Icon as={BiLogoWhatsapp} w={5} h={5} />
+            <Icon as={BiLogoTwitter} w={5} h={5} />
+          </Stack>
         </Stack>
       </Container>
     </Box>
-  )
-}
+  );
+};
 
 export default Footer;
---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box, Heading } from "@chakra-ui/react";
+
+const Navbar = () => {
+  return (
+    <Box>
+      <Heading>Navbar</Heading>
+    </Box>
+  );
+};
+export default Navbar;
