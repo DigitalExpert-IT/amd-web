6bad1cd15c6f3c5996f42bcd69546f1011284a7c
Y0SH4 <yosa.agung13@gmail.com>
2023-12-01 18:10:04 +0700

    update

--- src/components/PriceChart.tsx
+++ src/components/PriceChart.tsx
@@ -10,11 +10,17 @@ const PriceChart: React.FC<PriceChartProps> = ({ points }) => {
     <Box overflow="hidden" width="100%" height="100%">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 287 100">
         <polygon
+          fill="none"
+          stroke="#8884d8"
+          strokeWidth="2"
           points={`${points
             .map(p => `${p[0]},${p[1]}`)
             .join(" ")} 287,100 0,100`}
         />
-        <polyline points={points.map(p => `${p[0]},${p[1]}`).join(" ")} />
+        <polyline
+          fill="#8884d8"
+          points={points.map(p => `${p[0]},${p[1]}`).join(" ")}
+        />
       </svg>
     </Box>
   );
